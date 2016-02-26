b4680099a9d77102a069252d3891366a336b7ab1
raduran <raduran@paypal.com>
2016-02-26 12:08:22 -0700

    removed the need for a seperate results page and updated background

--- calcapp/calculator.py
+++ calcapp/calculator.py
@@ -10,8 +10,10 @@ app.secret_key = secret_key
 def home():
     """
     Home page of the app
-    Functionality for the calculations
-    POST will occur on the submit button
+    POST will check select fields and provide a rating as well as a link to send.
+    GET will check for query string parameters and calculate based on the values.
+    This allows us to avoid needing a data store when sharing our ratings.
+    If no values are provided on a GET the default priority will be 'Not Set'
     """
 
     form = FormOne()
@@ -47,51 +49,40 @@ def home():
               + str(ease) + "&awareness=" + str(awareness) + "&info=" + str(info) \
               + "&availability=" + str(availability) + "&brand=" + str(brand) + "&privacy=" + str(privacy)
 
-        return render_template('results.html', rating=rating, url=url)
+        return render_template('index.html', form=form, rating=rating, url=url)
+        # if GET then check if query string parameters to calculate
     else:
-        return render_template('index.html', form=form)
+        try:
+            level = int(request.args.get('level'))
+            discovery = int(request.args.get('discovery'))
+            ease = int(request.args.get('ease'))
+            awareness = int(request.args.get('awareness'))
+            info = int(request.args.get('info'))
+            availability = int(request.args.get('availability'))
+            brand = int(request.args.get('brand'))
+            privacy = int(request.args.get('privacy'))
 
+            # calculate score
+            some_factors = (level + discovery + ease + awareness) / 4.0
+            some_impact = (info + availability) / 2.0
+            other_impact = (brand + privacy) / 2.0
+            score = (some_factors + some_impact + other_impact) / 3.0
 
-@app.route('/results', methods=['GET'])
-def results():
-    """
-    loads results page for when providing a specific URL with query string parameters
+            # set priority
+            if score < 1.8:
+                rating = "bad"
+            elif score < 3.6:
+                rating = "meh"
+            elif score < 5.4:
+                rating = "good"
+            elif score < 7.2:
+                rating = "yum"
+            else:
+                rating = "desert island"
+        except:
+            rating = "Not Set"
 
-    example URL
-    http://127.0.0.1:8081/results?skill=1&discovery=3&exploit=5&awareness=6&classification=5&availability=5&brand=4&privacy=5
-    """
-
-    try:
-        level = int(request.args.get('level'))
-        discovery = int(request.args.get('discovery'))
-        ease = int(request.args.get('ease'))
-        awareness = int(request.args.get('awareness'))
-        info = int(request.args.get('info'))
-        availability = int(request.args.get('availability'))
-        brand = int(request.args.get('brand'))
-        privacy = int(request.args.get('privacy'))
-
-        # calculate score
-        some_factors = (level + discovery + ease + awareness) / 4.0
-        some_impact = (info + availability) / 2.0
-        other_impact = (brand + privacy) / 2.0
-        score = (some_factors + some_impact + other_impact) / 3.0
-
-        # set priority
-        if score < 1.8:
-            rating = "bad"
-        elif score < 3.6:
-            rating = "meh"
-        elif score < 5.4:
-            rating = "good"
-        elif score < 7.2:
-            rating = "yum"
-        else:
-            rating = "desert island"
-    except:
-        rating = "Not Set"
-
-    return render_template('results.html', rating=rating)
+        return render_template('index.html', form=form, rating=rating)
 
 
 @app.route('/test')
