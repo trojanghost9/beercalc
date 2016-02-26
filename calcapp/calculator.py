14f0075ccbb84b1052d191cd6a4282300732498b
raduran <raduran@paypal.com>
2016-02-26 13:22:41 -0700

    updated select fields and calculations to match beer terms

--- calcapp/calculator.py
+++ calcapp/calculator.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from settings import secret_key
-from forms import FormOne
+from forms import BeerForm
 
 app = Flask(__name__)
 app.secret_key = secret_key
@@ -16,73 +16,77 @@ def home():
     If no values are provided on a GET the default priority will be 'Not Set'
     """
 
-    form = FormOne()
+    form = BeerForm()
 
     if request.method == 'POST':
         # do the calculations and return the information on the results page
-        level = int(form.level_selector.data)
-        discovery = int(form.discovery_selector.data)
-        ease = int(form.ease_selector.data)
-        awareness = int(form.awareness_selector.data)
-        info = int(form.info_selector.data)
-        availability = int(form.availability_selector.data)
-        brand = int(form.brand_selector.data)
-        privacy = int(form.privacy_selector.data)
+        appearance = float(form.appearance_selector.data)
+        aroma = float(form.aroma_selector.data)
+        flavor = float(form.flavor_selector.data)
+        mouthfeel = float(form.mouthfeel_selector.data)
+        myappearance = float(form.myappearance_selector.data)
+        myaroma = float(form.myaroma_selector.data)
+        myflavor = float(form.myflavor_selector.data)
+        replay = float(form.replay_selector.data)
 
-        some_factors = (level + discovery + ease + awareness) / 4.0
-        some_impact = (info + availability) / 2.0
-        other_impact = (brand + privacy) / 2.0
-        score = (some_factors + some_impact + other_impact) / 3.0
+        # calculate score
+        style_factors = (appearance + aroma + flavor + mouthfeel) / 4.0
+        my_factors = (myappearance + myaroma + myflavor + replay) / 4.0
+        score = (style_factors + my_factors) / 2.0
 
-        if score < 1.8:
-            rating = "bad"
-        elif score < 3.6:
-            rating = "meh"
-        elif score < 5.4:
-            rating = "good"
-        elif score < 7.2:
-            rating = "yum"
+        if score < 2.0:
+            rating = "Poor it out!"
+        elif score < 4.0:
+            rating = "Meh"
+        elif score < 6.0:
+            rating = "Good"
+        elif score < 8.0:
+            rating = "Yum!"
         else:
-            rating = "desert island"
+            rating = "Desert island beer!!!"
 
-        url = "http://127.0.0.1:8081/results?level=" + str(level) + "&discovery=" + str(discovery) + "&ease=" \
-              + str(ease) + "&awareness=" + str(awareness) + "&info=" + str(info) \
-              + "&availability=" + str(availability) + "&brand=" + str(brand) + "&privacy=" + str(privacy)
+        url = "http://127.0.0.1:8081/?appearance=" + str(appearance) + "&aroma=" + str(aroma) + "&flavor=" \
+              + str(flavor) + "&mouthfeel=" + str(mouthfeel) + "&myappearance=" + str(myappearance) \
+              + "&myaroma=" + str(myaroma) + "&myflavor=" + str(myflavor) + "&replay=" + str(replay)
 
         return render_template('index.html', form=form, rating=rating, url=url)
         # if GET then check if query string parameters to calculate
     else:
         try:
-            level = int(request.args.get('level'))
-            discovery = int(request.args.get('discovery'))
-            ease = int(request.args.get('ease'))
-            awareness = int(request.args.get('awareness'))
-            info = int(request.args.get('info'))
-            availability = int(request.args.get('availability'))
-            brand = int(request.args.get('brand'))
-            privacy = int(request.args.get('privacy'))
+            appearance = float(request.args.get('appearance'))
+            aroma = float(request.args.get('aroma'))
+            flavor = float(request.args.get('flavor'))
+            mouthfeel = float(request.args.get('mouthfeel'))
+            myappearance = float(request.args.get('myappearance'))
+            myaroma = float(request.args.get('myaroma'))
+            myflavor = float(request.args.get('myflavor'))
+            replay = float(request.args.get('replay'))
 
             # calculate score
-            some_factors = (level + discovery + ease + awareness) / 4.0
-            some_impact = (info + availability) / 2.0
-            other_impact = (brand + privacy) / 2.0
-            score = (some_factors + some_impact + other_impact) / 3.0
+            style_factors = (appearance + aroma + flavor + mouthfeel) / 4.0
+            my_factors = (myappearance + myaroma + myflavor + replay) / 4.0
+            score = (style_factors + my_factors) / 2.0
 
-            # set priority
-            if score < 1.8:
-                rating = "bad"
-            elif score < 3.6:
-                rating = "meh"
-            elif score < 5.4:
-                rating = "good"
-            elif score < 7.2:
-                rating = "yum"
+            if score < 2.0:
+                rating = "Poor it out!"
+            elif score < 4.0:
+                rating = "Meh"
+            elif score < 6.0:
+                rating = "Good"
+            elif score < 8.0:
+                rating = "Yum!"
             else:
-                rating = "desert island"
+                rating = "Desert island beer!!!"
+
+            url = "http://127.0.0.1:8081/?appearance=" + str(appearance) + "&aroma=" + str(aroma) + "&flavor=" \
+                  + str(flavor) + "&mouthfeel=" + str(mouthfeel) + "&myappearance=" + str(myappearance) \
+                  + "&myaroma=" + str(myaroma) + "&myflavor=" + str(myflavor) + "&replay=" + str(replay)
+
         except:
             rating = "Not Set"
+            url = ""
 
-        return render_template('index.html', form=form, rating=rating)
+        return render_template('index.html', form=form, rating=rating, url=url)
 
 
 @app.route('/test')
--- calcapp/forms.py
+++ calcapp/forms.py
@@ -2,40 +2,26 @@ from flask_wtf import Form
 from wtforms import SubmitField, SelectField
 
 
-class FormOne(Form):
+class BeerForm(Form):
     # choice definitions
-    level = [(1, 'something'), (3, 'something'), (5, 'something'), (6, 'something'), (9, 'somethings')]
-
-    discovery = [(1, 'something'), (3, 'something'), (7, 'something'), (9, 'Asomething')]
-
-    ease = [(1, 'something'), (3, 'something'), (5, 'something'), (9, 'something')]
-
-    awareness = [(1, 'something'), (4, 'something'), (6, 'something'), (9, 'something')]
-
-    info = [(2, 'something'), (5, 'something'), (6, 'something'), (7, 'something'), (9, 'something')]
-
-    availability = [(1, 'something'), (5, 'something'), (5, 'something'), (7, 'something'), (9, 'something')]
-
-    brand = [(1, 'Minimal Damage'), (4, 'Loss of Major Accounts'), (5, 'Loss of Goodwill'), (9, 'Brand Damage')]
-
-    privacy = [(3, 'One Individual'), (5, 'Hundreds of People'), (7, 'Thousands of People'), (9, 'Millions of People')]
+    appearance = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
+    aroma = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
+    flavor = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
+    mouthfeel = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
+    myappearance = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
+    myaroma = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
+    myflavor = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
+    replay = [(1, 'something'), (3.25, 'something'), (5.5, 'something'), (7.75, 'something'), (10, 'somethings')]
 
     # selection drop downs
-    level_selector = SelectField("Level", choices=level)
-
-    discovery_selector = SelectField("Discovery", choices=discovery)
-
-    ease_selector = SelectField("Ease", choices=ease)
-
-    awareness_selector = SelectField("Awareness", choices=awareness)
-
-    info_selector = SelectField("Info", choices=info)
-
-    availability_selector = SelectField("Availability", choices=availability)
-
-    brand_selector = SelectField("Brand", choices=brand)
-
-    privacy_selector = SelectField("Privacy", choices=privacy)
+    appearance_selector = SelectField("Appearance", choices=appearance)
+    aroma_selector = SelectField("Aroma", choices=aroma)
+    flavor_selector = SelectField("Flavor", choices=flavor)
+    mouthfeel_selector = SelectField("Mouthfeel", choices=mouthfeel)
+    myappearance_selector = SelectField("Appearance", choices=myappearance)
+    myaroma_selector = SelectField("My Aroma", choices=myaroma)
+    myflavor_selector = SelectField("My Flavor", choices=myflavor)
+    replay_selector = SelectField("Replay", choices=replay)
 
     # Submit form.
     submit = SubmitField('Submit')
